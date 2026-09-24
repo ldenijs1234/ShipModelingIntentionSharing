fe31608a30a1ddf19cc8f5206f3a4c5a0bb76aab
ldenijs1234 <162971452+ldenijs1234@users.noreply.github.com>
2026-09-24 10:59:00 +0200

    a few runs

--- ros2_ws/run_batch_interval.sh
+++ ros2_ws/run_batch_interval.sh
@@ -6,7 +6,7 @@
 # Example: ./run_batch_interval.sh case05 15.0 3.0
 # ==============================================================================
 
-SCENARIO=("case04")
+SCENARIO=("case05")
 INTENT_RANGE=10.0
 SPEED_FACTOR=5.0
 TAU_FIXED=0.0
